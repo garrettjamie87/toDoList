9b3f132b84ae699895fbc06d041b7a0d9554aa6b
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-08 18:27:42 +0100

    add date.js as seperate component

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const date = require(__dirname + "/date.js")
+
 let items = [];
 
 app.set("view engine", "ejs");
@@ -13,15 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  
+let day = date();
 
   res.render("list", { kindOfDay: day, newListItems: items });
 });

25a9e9c83072f5c0f678cde92ce85a1f83fbc382
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-08 15:02:16 +0100

    add for loop to add to array

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+let items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
       let today = new Date();
@@ -17,53 +21,19 @@ app.get("/", function (req, res) {
 
       let day = today.toLocaleDateString("en-US", options);
 
-      res.render("list", {
-            kindOfDay: day
-      });
+      res.render("list", {kindOfDay: day, newListItems: items});
 
 });
 
+app.post("/", function( req, res) {
+   let item = req.body.newItem;
 
+   items.push(item);
 
+   res.redirect("/");
 
+});
 
-
-      // let currentDay = today.getDay();
-      // let day = '';
-
-      // switch (currentDay) {
-      //       case 0:
-      //             day = 'Sunday'
-
-      //             break;
-      //       case 1:
-      //             day = 'Monday'
-
-      //             break;
-      //       case 2:
-      //             day = 'Tuesday'
-
-      //             break;
-      //       case 3:
-      //             day = 'Wednesday'
-
-      //             break;
-      //       case 4:
-      //             day = 'Thursday'
-
-      //             break;
-      //       case 5:
-      //             day = 'Friday'
-
-      //             break;
-      //       case 6:
-      //             day = 'Saturday'
-
-      //             break;
-      //       default:
-      //             console.log(`there shouldn't be an 8th day`)
-      //             break;
-      // }
 
 
     

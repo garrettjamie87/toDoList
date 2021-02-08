f56c34abd7b1fba36b58d155405b92261e1c7d57
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-08 15:49:02 +0100

    add prelim styles

--- app.js
+++ app.js
@@ -6,38 +6,34 @@ const bodyParser = require("body-parser");
 const app = express();
 let items = [];
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-      let today = new Date();
+  let today = new Date();
 
-      let options = {
-            weekday: 'long',
-            day: 'numeric',
-            month: 'long'
-      };
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-      let day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
-      res.render("list", {kindOfDay: day, newListItems: items});
-
+  res.render("list", { kindOfDay: day, newListItems: items });
 });
 
-app.post("/", function( req, res) {
-   let item = req.body.newItem;
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
 
-   items.push(item);
+  items.push(item);
 
-   res.redirect("/");
-
+  res.redirect("/");
 });
-
-
-
-    
 
 app.listen(3000, function () {
-      console.log("Server started on port 3000.");
+  console.log("Server started on port 3000.");
 });

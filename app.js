0ab1a4bdac7d626cc2a907127afe263186891dac
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-08 12:59:32 +0100

    add switch statement to render day of week

--- app.js
+++ app.js
@@ -7,11 +7,50 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
-  
-            res.sendFile(__dirname + "/index.html")
+      let today = new Date();
+      let currentDay = today.getDay();
+      let day = '';
+
+      switch (currentDay) {
+            case 0:
+                  day = 'Sunday'
+
+                  break;
+            case 1:
+                  day = 'Monday'
+
+                  break;
+            case 2:
+                  day = 'Tuesday'
+
+                  break;
+            case 3:
+                  day = 'Wednesday'
+
+                  break;
+            case 4:
+                  day = 'Thursday'
+
+                  break;
+            case 5:
+                  day = 'Friday'
+
+                  break;
+            case 6:
+                  day = 'Saturday'
+
+                  break;
+            default:
+                  console.log(`there shouldn't be an 8th day`)
+                  break;
       }
 
-);
+
+      res.render("list", {
+            kindOfDay: day
+      });
+
+});
 
 app.listen(3000, function () {
       console.log("Server started on port 3000.");

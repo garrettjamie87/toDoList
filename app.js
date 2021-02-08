230174b326b1a59426c9c3525d35e577febcaf80
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-08 14:17:40 +0100

    refactor code using toLocaleDateString

--- app.js
+++ app.js
@@ -8,49 +8,65 @@ app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
       let today = new Date();
-      let currentDay = today.getDay();
-      let day = '';
 
-      switch (currentDay) {
-            case 0:
-                  day = 'Sunday'
+      let options = {
+            weekday: 'long',
+            day: 'numeric',
+            month: 'long'
+      };
 
-                  break;
-            case 1:
-                  day = 'Monday'
-
-                  break;
-            case 2:
-                  day = 'Tuesday'
-
-                  break;
-            case 3:
-                  day = 'Wednesday'
-
-                  break;
-            case 4:
-                  day = 'Thursday'
-
-                  break;
-            case 5:
-                  day = 'Friday'
-
-                  break;
-            case 6:
-                  day = 'Saturday'
-
-                  break;
-            default:
-                  console.log(`there shouldn't be an 8th day`)
-                  break;
-      }
-
+      let day = today.toLocaleDateString("en-US", options);
 
       res.render("list", {
             kindOfDay: day
       });
 
 });
+
+
+
+
+
+
+      // let currentDay = today.getDay();
+      // let day = '';
+
+      // switch (currentDay) {
+      //       case 0:
+      //             day = 'Sunday'
+
+      //             break;
+      //       case 1:
+      //             day = 'Monday'
+
+      //             break;
+      //       case 2:
+      //             day = 'Tuesday'
+
+      //             break;
+      //       case 3:
+      //             day = 'Wednesday'
+
+      //             break;
+      //       case 4:
+      //             day = 'Thursday'
+
+      //             break;
+      //       case 5:
+      //             day = 'Friday'
+
+      //             break;
+      //       case 6:
+      //             day = 'Saturday'
+
+      //             break;
+      //       default:
+      //             console.log(`there shouldn't be an 8th day`)
+      //             break;
+      // }
+
+
+    
 
 app.listen(3000, function () {
       console.log("Server started on port 3000.");

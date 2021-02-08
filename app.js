e5dba7e6383df2b2560226a397d5ae86e51c14d7
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-08 12:26:46 +0100

    add view engine -ejs

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
-  res.send("Hello");
-});
+app.get("/", function (req, res) {
+  
+            res.sendFile(__dirname + "/index.html")
+      }
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+);
+
+app.listen(3000, function () {
+      console.log("Server started on port 3000.");
 });
